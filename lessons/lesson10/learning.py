e281179d3ac2f21565d9bb099f8d264883f8b123
heychi9487 <raylee931110@gmail.com>
2024-08-01 06:34:30 +0000

    omg30 files

---
+++ lessons/lesson10/learning.py
@@ -0,0 +1,57 @@
+import random
+class Person():            #初始化執行需要括號
+    def __init__(self,n:str,h:int,w:int):
+        self._name = n
+        self._height = h
+        self._weight = w
+    
+    @staticmethod                                  #把method放進class裡要用的東西
+    def getPerson(name:str):
+        height = random.randint(150,190)
+        weight = random.randint(50,90)
+        return Person(n=name,h=height,w=weight)
+    
+    @classmethod                                   #把method放進class裡要用的東西
+    def getPerson1(cls,name:str):                  # cls 就是指class的意思，知道要加即可
+        height = random.randint(150,190)
+        weight = random.randint(50,90)
+        return Person(n=name,h=height,w=weight)
+
+    @property
+    def name(self) -> str:
+        return self._name
+    
+    @property
+    def height(self)->int:
+        return self._height
+    
+    @property
+    def weight(self)->int:
+        return self._weight
+
+    def getBmi(self) -> float:
+        return self.weight / (self.height/100) ** 2
+    
+    def get_status(self) -> str:
+        bmi:float = self.getBmi()
+        if bmi >= 35:
+            return '重度肥胖'
+        elif bmi >= 30:
+            return '中度肥胖'
+        elif bmi >= 27:
+            return '輕度肥胖'
+        elif bmi >= 24:
+            return '體重過重'
+        elif bmi >= 18.5:
+            return '體重正常'
+        else:
+            return '體重過輕'
+        
+    def bmi_print(self)->str:
+        return f"{self.name}你好\n身高是：{self.height}公分\n體重是：{self.weight}公斤\nBMI:{round(self.getBmi(),ndigits=2)}\n{self.get_status()}"
+
+def getPerson(name:str) -> Person:
+    height = random.randint(150,190)
+    weight = random.randint(50,90)
+    return Person(n=name,h=height,w=weight)
+
